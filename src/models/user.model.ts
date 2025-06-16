0f6b61b32d858d4b6bdce0e6f823d696e1985680
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-16 17:52:10 +0300

    feat(user): add User model, schema, and interfaces with phone, address and profile schemas

---
+++ src/models/user.model.ts
@@ -0,0 +1,46 @@
+import {model, Schema} from "mongoose";
+import {Address, IUserDocument, Phone, Profile} from "../core/interfaces/user.interfaces";
+import {ANNOUNCEMENT_MODEL_NAME, ROLE_MODEL_NAME, TICKET_MODEL_NAME} from "../core/interfaces/role.interfaces";
+
+
+export const PhoneSchema = new Schema<Phone>({
+    type: {type: String, required: true},
+    phone: {type: String, required: true},
+}, {_id: false});
+
+export const AddressSchema = new Schema<Address>({
+    street: {type: String, required: true},
+    number: {type: String, required: true},
+    city: {type: String, required: true},
+    zipCode: {type: String, required: true},
+}, {_id: false});
+
+export const ProfileSchema = new Schema<Profile>({
+    firstname: {type: String},
+    lastname: {type: String},
+    avatar: {type: String},
+    phones: {
+        type: [PhoneSchema], default: []
+    },
+    address: AddressSchema
+}, {_id: false});
+
+export const UserSchema = new Schema<IUserDocument>({
+    email: {type: String, required: true, unique: true},
+    vat: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    enabled: {type: Boolean, required:true, default: false},
+    verified: {type: Boolean, required:true,  default: false},
+    loginConsecutiveFailures: {type: Number, required:true, default: 0},
+    passwordChangedAt: {type: Date, required: true},
+    passwordResetToken: {type: String},
+    passwordResetTokenExpires: {type: Date},
+    role: {type: Schema.Types.ObjectId, ref: ROLE_MODEL_NAME, required: true},
+    profile: {type: ProfileSchema, default: null},
+    tickets: {type: [Schema.Types.ObjectId], ref: TICKET_MODEL_NAME, default: []},
+    announcements: {type: [Schema.Types.ObjectId], ref: ANNOUNCEMENT_MODEL_NAME, default: []},
+    }, {
+    timestamps: true
+})
+
+export const User = model<IUserDocument>("User", UserSchema);
