81bb451d27fcd7cb8444f4f5bcc0f4566bc9268e
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-16 13:53:53 +0300

    feat(role): add Role model, schema, and interfaces with authority actions and resource enums

---
+++ src/core/interfaces/role.interfaces.ts
@@ -0,0 +1,25 @@
+import {Document} from "mongoose";
+
+export const ROLE_MODEL_NAME = "Role"
+export const ANNOUNCEMENT_MODEL_NAME = "Announcement";
+export const USER_MODEL_NAME = "User"
+export const TICKET_MODEL_NAME = "Ticket"
+export const ATTACHMENT_MODEL_NAME = "Attachment"
+export const AUTHORITY_ACTIONS = ['READ', 'CREATE', 'UPDATE', 'DELETE'];
+export const RESOURCES = [USER_MODEL_NAME, TICKET_MODEL_NAME, ROLE_MODEL_NAME, ANNOUNCEMENT_MODEL_NAME, ATTACHMENT_MODEL_NAME];
+
+export type AuthorityAction = typeof AUTHORITY_ACTIONS[number];
+export type ResourceAction = typeof RESOURCES[number];
+
+export interface Authority {
+    resource: ResourceAction;
+    actions: AuthorityAction[];
+}
+
+
+export interface IRoleDocument extends Document {
+    name: string;
+    authorities: Authority[];
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ src/models/role.model.ts
@@ -0,0 +1,31 @@
+import {model, Schema} from "mongoose";
+import {
+    Authority,
+    AUTHORITY_ACTIONS,
+    IRoleDocument,
+    RESOURCES,
+    ROLE_MODEL_NAME
+} from "../core/interfaces/role.interfaces";
+
+export const AuthoritySchema = new Schema<Authority>({
+    resource: {
+        type: String,
+        enum: RESOURCES,
+        required: true,
+    },
+    actions: {
+        type: [String],
+        enum: AUTHORITY_ACTIONS,
+        default: []
+    },
+}, {_id: false});
+
+export const RoleSchema = new Schema<IRoleDocument>({
+    name: {type: String, required: true, unique: true},
+    authorities: {
+        type: [AuthoritySchema],
+        default: []
+    },
+}, {timestamps: true});
+
+export const Role = model<IRoleDocument>(ROLE_MODEL_NAME, RoleSchema);
