d632f5731e872bc3ee98b1fbec9ec1f062628f1e
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-16 13:54:43 +0300

    feat(user): add User interfaces with with profile, role, and validation fields

---
+++ src/core/interfaces/user.interfaces.ts
@@ -0,0 +1,42 @@
+import {Document, Types} from 'mongoose';
+import {ITicketDocument} from "./ticket.interfaces";
+import {IAnnouncementDocument} from "./announcement.interfaces";
+import {IRoleDocument} from "./role.interfaces";
+
+export interface Phone {
+    type: string;
+    phone: string;
+}
+
+export interface Address {
+    street: string;
+    number: string;
+    city: string;
+    zipCode: string;
+}
+
+export interface Profile {
+    firstname?: string;
+    lastname?: string;
+    avatar?: string;
+    phoneNumber?: Phone[];
+    address?: Address;
+}
+
+export interface IUserDocument extends Document {
+    email: string;
+    vat: string;
+    password: string;
+    enabled: boolean;
+    verified: boolean;
+    loginConsecutiveFailures: number;
+    passwordChangedAt: Date;
+    passwordResetToken?: string;
+    passwordResetExpires?: Date;
+    role: Types.ObjectId | IRoleDocument;
+    createdAt: Date;
+    updatedAt: Date;
+    profile?: Profile;
+    tickets?: Types.ObjectId[] | ITicketDocument[];
+    announcements?: Types.ObjectId[] | IAnnouncementDocument[];
+}
