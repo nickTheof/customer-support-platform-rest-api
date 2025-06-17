3aed1dad6035ec45853df947474008c454455844
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-17 23:06:17 +0300

    feat(security): add methods for generating secure tokens for supporting verification, reset-password and unlocking of a user

--- src/core/utils/security.ts
+++ src/core/utils/security.ts
@@ -1,5 +1,6 @@
 import env_config from "../env_config";
 import bcrypt from "bcrypt";
+import * as crypto from "node:crypto";
 
 const hashPassword = async(password: string) => {
     const salt = await bcrypt.genSalt(env_config.SALT_ROUNDS);
@@ -10,7 +11,25 @@ const comparePassword = (plainPassword: string, storedPassword: string) => {
     return bcrypt.compare(plainPassword, storedPassword);
 }
 
+const generateVerificationToken = () => {
+    const verificationToken = crypto.randomBytes(64).toString("hex");
+    return crypto.createHash("sha256").update(verificationToken).digest("hex");
+}
+
+const generateResetPasswordToken = () => {
+    const resetToken = crypto.randomBytes(64).toString("hex");
+    return crypto.createHash("sha256").update(resetToken).digest("hex");
+}
+
+const generateEnableUserToken = () => {
+    const enableUserToken = crypto.randomBytes(64).toString("hex");
+    return crypto.createHash("sha256").update(enableUserToken).digest("hex");
+}
+
 export {
     hashPassword,
     comparePassword,
+    generateVerificationToken,
+    generateResetPasswordToken,
+    generateEnableUserToken,
 }
