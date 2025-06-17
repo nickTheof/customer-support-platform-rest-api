73ddbd492ccfdd6497dc8784745022003db66b19
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-17 23:00:34 +0300

    feat(types): infer types from zod schemas for authentication DTOs

--- src/core/types/zod-model.types.ts
+++ src/core/types/zod-model.types.ts
@@ -1,8 +1,20 @@
 import {SaveAttachmentSchema} from "../../schemas/attachment.schemas";
 import {AnnouncementInsertDTOSchema} from "../../schemas/announcement.schemas";
 import {TicketInsertDTOSchema, CommentInsertDTOSchema} from "../../schemas/ticket.schemas";
-import {RoleInsertDTOSchema, AuthorityInsertDTOSchema} from "../../schemas/role.schemas";
-import {PhoneInsertDTOSchema, AddressInsertDTOSchema, ProfileInsertDTOSchema, UserInsertDTOSchema, UserRegisterDTOSchema} from "../../schemas/user.schemas";
+import {RoleInsertDTOSchema, AuthorityInsertDTOSchema, RoleReadOnlyDTOSchema} from "../../schemas/role.schemas";
+import {
+    PhoneInsertDTOSchema,
+    AddressInsertDTOSchema,
+    ProfileInsertDTOSchema,
+    UserInsertDTOSchema,
+    UserRegisterDTOSchema,
+    UserLoginDTOSchema,
+    ResetPasswordDTOSchema,
+    BaseUserReadOnlyDTOSchema,
+    UserReadOnlyDTOSchemaWithVerificationToken,
+    BaseUserReadOnlyDTOSchemaWithRole,
+    VerifyAccountDTOSchema, UnlockAccountDTOSchema
+} from "../../schemas/user.schemas";
 import {z} from "zod/v4";
 
 export type SaveAttachmentDTO = z.infer<typeof SaveAttachmentSchema>;
@@ -16,3 +28,11 @@ export type AddressInsertDTO = z.infer<typeof AddressInsertDTOSchema>;
 export type ProfileInsertDTO = z.infer<typeof ProfileInsertDTOSchema>;
 export type UserInsertDTO = z.infer<typeof UserInsertDTOSchema>;
 export type UserRegisterDTO = z.infer<typeof UserRegisterDTOSchema>;
+export type UserLoginDTO = z.infer<typeof UserLoginDTOSchema>;
+export type ResetPasswordDTO = z.infer<typeof ResetPasswordDTOSchema>;
+export type BaseUserReadOnlyDTO = z.infer<typeof BaseUserReadOnlyDTOSchema>;
+export type BaseUserReadOnlyDTOWithVerification = z.infer<typeof UserReadOnlyDTOSchemaWithVerificationToken>;
+export type BaseUserReadOnlyDTOWithRole = z.infer<typeof BaseUserReadOnlyDTOSchemaWithRole>
+export type VerifyAccountDTO = z.infer<typeof VerifyAccountDTOSchema>;
+export type RoleReadOnlyDTO = z.infer<typeof RoleReadOnlyDTOSchema>;
+export type UnlockAccountDTO = z.infer<typeof UnlockAccountDTOSchema>;
