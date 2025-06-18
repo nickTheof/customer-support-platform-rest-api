ee15aa832cd37e8aed123afb8de2a0e00f5ad45e
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-19 00:19:07 +0300

    fix: remove unused types

--- src/core/types/config.types.ts
+++ src/core/types/config.types.ts
@@ -1,10 +1,4 @@
 import {z} from "zod/v4";
-import {bcryptSchema, corsSchema, jwtSchema, envSchema, limiterSchema, mailerSchema, mongoSchema} from '../../schemas/env.schemas'
+import { envSchema } from '../../schemas/env.schemas'
 
 export type ConfigType = z.infer<typeof envSchema>;
-export type LimiterConfigType = z.infer<typeof limiterSchema>;
-export type CorsConfigType = z.infer<typeof corsSchema>;
-export type BCryptConfigType = z.infer<typeof bcryptSchema>;
-export type JwtConfigType = z.infer<typeof jwtSchema>;
-export type MailerConfigType = z.infer<typeof mailerSchema>;
-export type MongoDBConnectionType = z.infer<typeof mongoSchema>;
