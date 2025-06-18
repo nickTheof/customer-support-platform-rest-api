cfde0211057126efba995edefd7e7eb360df09d9
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-18 18:32:56 +0300

    feat(mapper): add method for mapping User Document to ReadOnlyDTO which is used by the user service layer

--- src/mapper/mapper.ts
+++ src/mapper/mapper.ts
@@ -1,8 +1,8 @@
-import {IUserDocument} from "../core/interfaces/user.interfaces";
+import {IUserDocument, Profile} from "../core/interfaces/user.interfaces";
 import {
     BaseUserReadOnlyDTO,
     BaseUserReadOnlyDTOWithRole,
-    BaseUserReadOnlyDTOWithVerification
+    BaseUserReadOnlyDTOWithVerification, UserReadOnlyDTO
 } from "../core/types/zod-model.types";
 import {IRoleDocument} from "../core/interfaces/role.interfaces";
 
@@ -33,9 +33,25 @@ const mapUserToBaseUserDTOWithVerificationCredentials = (user: IUserDocument): B
     }
 }
 
+const mapUserToReadOnlyDTO = (user: IUserDocument): UserReadOnlyDTO => {
+    const userRole = user.role as IRoleDocument;
+    const profile = user.profile as Profile;
+    const mappedRole = mapRoleToReadOnlyDTO(userRole);
+    return {
+        _id: user._id.toString(),
+        email: user.email,
+        enabled: user.enabled,
+        profile: profile,
+        role: mappedRole,
+        vat: user.vat,
+        verified: user.verified
+    }
+}
+
 export default {
     mapUserToBaseUserDTO,
     mapRoleToReadOnlyDTO,
     mapUserToBaseUserDTOWithVerificationCredentials,
-    mapUserToBaseUserDTOWithRole
+    mapUserToBaseUserDTOWithRole,
+    mapUserToReadOnlyDTO
 }
