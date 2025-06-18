86e10ba255b6e34f10efc792b530414e5e1c438b
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-18 12:09:18 +0300

    fix(role): change in generating type of AuthorityAction, ResourceAction

--- src/core/interfaces/role.interfaces.ts
+++ src/core/interfaces/role.interfaces.ts
@@ -8,8 +8,13 @@ export const ATTACHMENT_MODEL_NAME = "Attachment"
 export const AUTHORITY_ACTIONS = ['READ', 'CREATE', 'UPDATE', 'DELETE'];
 export const RESOURCES = [USER_MODEL_NAME, TICKET_MODEL_NAME, ROLE_MODEL_NAME, ANNOUNCEMENT_MODEL_NAME, ATTACHMENT_MODEL_NAME];
 
-export type AuthorityAction = typeof AUTHORITY_ACTIONS[number];
-export type ResourceAction = typeof RESOURCES[number];
+export type AuthorityAction = 'READ' | 'CREATE' | 'UPDATE' | 'DELETE';
+export type ResourceAction =
+    | typeof ROLE_MODEL_NAME
+    | typeof ANNOUNCEMENT_MODEL_NAME
+    | typeof USER_MODEL_NAME
+    | typeof TICKET_MODEL_NAME
+    | typeof ATTACHMENT_MODEL_NAME;
 
 export interface Authority {
     resource: ResourceAction;
