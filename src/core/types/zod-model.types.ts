fdee016815ad3c68a9450c8b288f01dfb075fdee
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-19 00:20:33 +0300

    feat(role): add zod schemas for CRUD operation in Role entity - Infer types of Role DTOs from zod schemas

--- src/core/types/zod-model.types.ts
+++ src/core/types/zod-model.types.ts
@@ -1,11 +1,13 @@
 import {SaveAttachmentSchema} from "../../schemas/attachment.schemas";
 import {AnnouncementInsertDTOSchema} from "../../schemas/announcement.schemas";
 import {TicketInsertDTOSchema, CommentInsertDTOSchema} from "../../schemas/ticket.schemas";
-import {RoleInsertDTOSchema, AuthorityInsertDTOSchema, RoleReadOnlyDTOSchema} from "../../schemas/role.schemas";
 import {
-    PhoneInsertDTOSchema,
-    AddressInsertDTOSchema,
-    ProfileInsertDTOSchema,
+    RoleInsertDTOSchema,
+    AuthorityInsertDTOSchema,
+    RoleReadOnlyDTOSchema,
+    RolePatchDTOSchema, RoleUpdateDTOSchema
+} from "../../schemas/role.schemas";
+import {
     UserInsertDTOSchema,
     UserRegisterDTOSchema,
     UserLoginDTOSchema,
@@ -23,27 +25,48 @@ import {
 } from "../../schemas/user.schemas";
 import {z} from "zod/v4";
 
-export type SaveAttachmentDTO = z.infer<typeof SaveAttachmentSchema>;
-export type AnnouncementInsertDTO = z.infer<typeof AnnouncementInsertDTOSchema>;
-export type TicketInsertDTO = z.infer<typeof TicketInsertDTOSchema>;
-export type CommentInsertDTO = z.infer<typeof CommentInsertDTOSchema>;
+// ROLE DTO TYPES
 export type RoleInsertDTO = z.infer<typeof RoleInsertDTOSchema>;
+export type RoleReadOnlyDTO = z.infer<typeof RoleReadOnlyDTOSchema>;
+export type RoleUpdateDTO = z.infer<typeof RoleUpdateDTOSchema>;
+export type RolePatchDTO = z.infer<typeof RolePatchDTOSchema>;
 export type AuthorityInsertDTO = z.infer<typeof AuthorityInsertDTOSchema>;
-export type PhoneInsertDTO = z.infer<typeof PhoneInsertDTOSchema>;
-export type AddressInsertDTO = z.infer<typeof AddressInsertDTOSchema>;
-export type ProfileInsertDTO = z.infer<typeof ProfileInsertDTOSchema>;
+
+
+
+//AUTHENTICATION SERVICE DTO TYPES
 export type UserInsertDTO = z.infer<typeof UserInsertDTOSchema>;
 export type UserRegisterDTO = z.infer<typeof UserRegisterDTOSchema>;
 export type UserLoginDTO = z.infer<typeof UserLoginDTOSchema>;
 export type ResetPasswordDTO = z.infer<typeof ResetPasswordDTOSchema>;
+export type VerifyAccountDTO = z.infer<typeof VerifyAccountDTOSchema>;
+export type UnlockAccountDTO = z.infer<typeof UnlockAccountDTOSchema>;
+
+
+//USER SERVICE DTO TYPES
 export type BaseUserReadOnlyDTO = z.infer<typeof BaseUserReadOnlyDTOSchema>;
 export type BaseUserReadOnlyDTOWithVerification = z.infer<typeof UserReadOnlyDTOSchemaWithVerificationToken>;
 export type BaseUserReadOnlyDTOWithRole = z.infer<typeof BaseUserReadOnlyDTOSchemaWithRole>
-export type VerifyAccountDTO = z.infer<typeof VerifyAccountDTOSchema>;
-export type RoleReadOnlyDTO = z.infer<typeof RoleReadOnlyDTOSchema>;
-export type UnlockAccountDTO = z.infer<typeof UnlockAccountDTOSchema>;
 export type UserReadOnlyDTO = z.infer<typeof UserReadOnlyDTOSchema>;
 export type UserUpdateDTO = z.infer<typeof UserUpdateDTOSchema>;
 export type UserPatchDTO = z.infer<typeof UserPatchDTOSchema>;
 export type UpdateUserRoleDTO = z.infer<typeof UpdateUserRoleDTOSchema>;
 export type FilterPaginationUsersDTO = z.infer<typeof FilterPaginationUserSchema>
+
+
+//ATTACHMENT DTO TYPES
+export type SaveAttachmentDTO = z.infer<typeof SaveAttachmentSchema>;
+
+//ANNOUNCEMENT DTO TYPES
+export type AnnouncementInsertDTO = z.infer<typeof AnnouncementInsertDTOSchema>;
+
+//TICKET DTO TYPES
+export type TicketInsertDTO = z.infer<typeof TicketInsertDTOSchema>;
+export type CommentInsertDTO = z.infer<typeof CommentInsertDTOSchema>;
+
+
+
+
+
+
+
--- src/schemas/role.schemas.ts
+++ src/schemas/role.schemas.ts
@@ -1,5 +1,6 @@
 import {z} from "zod/v4"
 import {AUTHORITY_ACTIONS, RESOURCES} from "../core/interfaces/role.interfaces";
+import {objectIdSchema} from "./user.schemas";
 
 export const AuthorityInsertDTOSchema = z.object({
     resource: z.enum(RESOURCES),
@@ -12,3 +13,17 @@ export const RoleInsertDTOSchema = z.object({
 })
 
 export const RoleReadOnlyDTOSchema = RoleInsertDTOSchema.extend({});
+
+export const RoleIdPathSchema = z.object({
+    id: objectIdSchema
+})
+
+export const RoleUpdateDTOSchema = RoleInsertDTOSchema.extend({});
+
+export const RolePatchDTOSchema = z.object({
+    name: z.string().nonempty().optional(),
+    authorities: z.array(AuthorityInsertDTOSchema).optional(),
+}).refine(
+    data => Object.keys(data).length > 0,
+    { message: "At least one field must be provided." }
+);
